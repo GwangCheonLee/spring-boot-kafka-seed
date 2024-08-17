178b78246ed561a0a534dc8e649f7348aec06e8f
GwangCheonLee <raitmid56@gmail.com>
2024-08-17 09:15:16 +0900

    project init

---
+++ src/main/java/com/example/kafkaseed/KafkaSeedApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaseed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kafkaseed/KafkaSeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaseed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
